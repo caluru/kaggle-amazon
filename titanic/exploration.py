327c6fd1e784c863ee55bee51f7a232d4522a2dc
Chaitanya Aluru <chaitanya.aluru@gmail.com>
2017-07-21 01:26:39 -0400

    Added graphs of survival rates by class, as well as code to print statistics of Age and Ticket Class

--- titanic/exploration.py
+++ titanic/exploration.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
@@ -9,12 +11,14 @@ import seaborn as sns
 #Changes to seaborn theme (Comment this line to see the difference!)
 sns.set()
 
+#Read in data
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 gender = pd.read_csv('gender_submission.csv')
 
-#replace nans with -1
-train['Age'].fillna(value=-20, inplace=True) 
+#replace nans (Not sure if this is the best way to deal with them, some have a lot of nans
+train['Age'].fillna(value=-20, inplace=True) #177 / 891 are nan
+train['Cabin'].fillna(value='Z', inplace=True) #687 / 891 are nan...
 
 #separate training data for living and dead people
 lived = train.loc[train['Survived'] == 1]
@@ -35,5 +39,40 @@ plt.figure()
 sns.distplot(died['Age'])
 plt.title('Age Distribution of Dead Passengers')
 
+print "Ratio of survivors to total passengers by age"
+
+boundaries = range(0,int(max(train['Age'])) + 10, 5)
+
+for i in range(len(boundaries) - 1):
+	lower = boundaries[i]
+	upper = boundaries[i+1]
+	live = len(lived[(lived.Age >= lower) & (lived.Age < upper)])
+	total = len(train[(train.Age >= lower) & (train.Age < upper)])
+	if total > 0:
+		print (boundaries[i], boundaries[i+1]), live / total
+
+print "----------------------------------------------------"
+
 #Show plots, erase history
 plt.show()
+
+#Looking at effect of ticket class on survival rates
+sns.countplot(train['Pclass'])
+plt.title('Class distribution of all passengers')
+plt.figure()
+
+sns.countplot(lived['Pclass'])
+plt.title('Class distribution of surviving passengers')
+plt.figure()
+
+sns.countplot(died['Pclass'])
+plt.title('Class distribution of deceased passengers')
+
+plt.show()
+
+print "Ratio of survived to total num passengers for each ticket class"
+
+for cls in [1,2,3]:
+	print cls, len(lived.loc[lived['Pclass'] == cls]) / len(train.loc[train['Pclass'] == cls])
+
+#Cabin information
