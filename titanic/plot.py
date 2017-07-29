20fc6ee464104e4841184e4e88c28050858713d8
pshanker7 <pshanker@umich.edu>
2017-07-29 14:02:50 -0400

    Update plot.py

--- titanic/plot.py
+++ titanic/plot.py
@@ -3,23 +3,23 @@ from pandas import DataFrame,Series
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set() #Using seaborn :) 
 
 df= pd.read_csv('kaggle_titanic/train.csv', sep = '\s*,\s*', na_values= ".",engine = 'python')
 print (df)    #print data
 (df['Name']) #test selector :)
 print(df.columns.tolist()) #Print only columns
 df.shape
+df['Age'].fillna(value=-1,inplace=True)
 
-#Visualize Data Using Scatterplot
-#Plot 1: Survival,Pclass
-#Plot 2: Survival, Age
-#Plot 3: Survival, Fare Paid
-groups = df.groupby('Survived')
-fig,ax =plt.subplots()
-for name,group in groups:
-    ax.plot(group.Age,group.Survived,marker = 'o',linestyle=' ',ms=12,label = name)
-ax.legend()
+sns.distplot(df['Age'])
+plt.title('Age Distribution of all passengers')
+plt.show()  
+
+#Create Scatterplots for Various Combinations in the Dataset
+#Age vs Survived
+sns.lmplot(x='Age', y= 'PassengerID', data=df)
+plt.title('Survived vs Age')
+plt.figure()
 plt.show()
-
-#TODO: Fix Indexing
-#TODO: Add linear regression model
